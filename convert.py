e482a1c0683c8521404d9378f1812fd232725498
Emil VATAI <emil.vatai@gmail.com>
2021-11-03 22:51:41 +0900

    Clean up `convert.py`

--- convert.py
+++ convert.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
+
 import collections
+import shutil
+import sys
 from pathlib import Path
 
 import torch
-import torch.nn
-from transformers import (BertConfig, BertForMaskedLM,
-                          BertForSequenceClassification, BertModel)
 
 
 def rename(key: str, last_layer_idx: int):
@@ -50,64 +50,29 @@ def get_last_layer_idx(keys):
     return max(nums)
 
 
-def fix_hf_state_dict(state_dict):
-    # state_dict["bert.embeddings.word_embeddings.weight"] = state_dict[
-    #     "bert.embeddings.word_embeddings.weight"
-    # ][:-6]
-    # # state_dict["classifier.weight"] = state_dict["classifier.weight"][:2, :]
-    # # state_dict["classifier.bias"] = state_dict["classifier.bias"][:2]
-    # state_dict["cls.predictions.decoder.weight"] = state_dict[
-    #     "cls.predictions.decoder.weight"
-    # ][:-6]
-    # state_dict["cls.predictions.bias"] = state_dict["cls.predictions.bias"][:-6]
-    # del state_dict["cls.predictions.transform.dense.weight"]
-    # del state_dict["cls.predictions.transform.dense.bias"]
-    # emb_dim = state_dict["bert.embeddings.position_embeddings.weight"].shape[0]
-    # # state_dict["bert.embeddings.position_ids"] = torch.zeros(1, emb_dim)
-    # state_dict["cls.predictions.decoder.bias"] = state_dict["cls.predictions.bias"]
-    # del state_dict["bert.pooler.dense.weight"]
-    # del state_dict["bert.pooler.dense.bias"]
-    pass
-
-
 def main():
-    path = Path("/mnt/second/pretraining_experiment-/epoch1000000_step303")
-    pt_file = path / "pytorch_model.bin"
+    assert len(sys.argv) == 2
+    input_path = Path(sys.argv[1])
+    pt_file = input_path / "pytorch_model.bin"
     ab_state_dict = torch.load(pt_file)
 
-    # w0 = ab_state_dict["bert.embeddings.LayerNorm.weight"]
-    # b0 = ab_state_dict["bert.embeddings.LayerNorm.bias"]
-    # w1 = ab_state_dict["bert.encoder.layer.0.PreAttentionLayerNorm.weight"]
-    # b1 = ab_state_dict["bert.encoder.layer.0.PreAttentionLayerNorm.bias"]
-    # print("w0 min max:", w0.min().item(), w0.max().item())
-    # print("b0 min max:", b0.min().item(), b0.max().item())
-    # print("w1 min max:", w1.min().item(), w1.max().item())
-    # print("b1 min max:", b1.min().item(), b1.max().item())
     idx = get_last_layer_idx(ab_state_dict.keys())
     kv_pairs = [(rename(k, idx), v) for k, v in ab_state_dict.items() if keep(k)]
     hf_state_dict = collections.OrderedDict(kv_pairs)
-    # fix_hf_state_dict(hf_state_dict)
 
-    torch.save(hf_state_dict, "/tmp/help/pytorch_model.bin")
-    # config = BertConfig.from_json_file(path / "config.json")
-    config = BertConfig.from_pretrained("bert-base-uncased")
-    # model = BertForSequenceClassification(config)
-    # model = BertForMaskedLM(config)
-    model = BertForSequenceClassification.from_pretrained("/tmp/help")
-    model = BertForSequenceClassification.from_pretrained("bert-base-uncased")
-    # grep for pooler and choose most reasonable one
-    # - BertForMaskedLM
-    # - BertModel
-
-    # for k in list(ab_state_dict.keys())[-10:]:
-    #     print(k)
-
-    # model.load_state_dict(hf_state_dict)
-    # model.save_pretrained(path / "conv2hf")
-
-    # print(model)
+    output_path = input_path / "convert2hf"
+    output_path.mkdir(exist_ok=True)
+    for file_name in [
+        "special_tokens_map.json",
+        "vocab.txt",
+        "config.json",
+        "tokenizer_config.json",
+    ]:
+        shutil.copy(input_path / file_name, output_path / file_name)
+    torch.save(hf_state_dict, output_path / "pytorch_model.bin")
 
     print("DONE")
 
 
-main()
+if __name__ == "__main__":
+    main()
