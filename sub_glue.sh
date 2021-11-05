e3198c2bdd0a231a7ee1e1ca0863c60b02237b7b
Emil VATAI <emil.vatai@gmail.com>
2021-11-05 13:12:32 +0900

    Update `sub_glue.sh`

--- sub_glue.sh
+++ sub_glue.sh
@@ -4,6 +4,7 @@
 #$ -j y
 #$ -l h_rt=2:30:00
 #$ -cwd
+#$ -o ~/shared/data/NLP/academic-budget-ckpt/$JOB_NAME-$JOB_ID/output.txt
 
 source /etc/profile.d/modules.sh
 module load gcc/9.3.0 python/3.8/3.8.7 openmpi/4.0.5 cuda/11.1/11.1.1 cudnn/8.0/8.0.5 nccl/2.8/2.8.4-1
@@ -14,9 +15,8 @@ NUM_PROCS=$(expr ${NHOSTS} \* ${NUM_GPUS_PER_NODE})
 
 MPIOPTS="-np ${NUM_PROCS} -map-by ppr:${NUM_GPUS_PER_NODE}:node -mca pml ob1 -mca btl self,tcp -mca btl_tcp_if_include bond0"
 
-
-python run_glue.py \
-  --model_name_or_path ~/share/data \
+python3 run_glue.py \
+  --model_name_or_path ~/shared/data/NLP/academic-budget-ckpt \
   --task_name MNLI \
   --max_seq_length 128 \
   --output_dir ~/data/data/NLP/academic-training-short-ckpt-ft \
