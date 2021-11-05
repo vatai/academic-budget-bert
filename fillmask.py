8e7d3a4d338b48c6b9b9b4255ac4b1d1c6a4eb99
Emil VATAI <emil.vatai@gmail.com>
2021-11-05 14:38:03 +0900

    Update `fillmask.py`

--- fillmask.py
+++ fillmask.py
@@ -1,16 +1,16 @@
-from transformers import pipeline, AutoTokenizer, PretrainedConfig, BertForMaskedLM
+from pathlib import Path
+
+import torch
+from transformers import AutoTokenizer, PretrainedConfig
 
 from pretraining.modeling import BertLMHeadModel
 
-
 ft_ckpt_dir = "/groups/gcb50300/data/NLP/academic-training-large-ckpt/pretraining_experiment-/epoch1000000_step14823/"
+ft_ckpt_dir = Path(ft_ckpt_dir)
 
-config = PretrainedConfig.from_pretrained(ft_ckpt_dir)
-model = BertLMHeadModel(config, args=ft_ckpt_dir)
-model.from_pretrained(ft_ckpt_dir, args=ft_ckpt_dir)
-model.__class__ = BertForMaskedLM
-print("MODEL NAME", model.__class__)
-tokenizer = AutoTokenizer.from_pretrained(ft_ckpt_dir)
+if not ft_ckpt_dir.exists():
+    ft_ckpt_dir = Path("/mnt/second/pretraining_experiment-/large")
+
 
 def get_queries(mask_token):
     queries = [
@@ -21,16 +21,31 @@ def get_queries(mask_token):
     return queries
 
 
-def fill_mask(model, tokenizer, query):
-    fill_mask = pipeline(
-        "fill-mask",
-        model=model,
-        tokenizer=tokenizer)
-    result = fill_mask(query)
-    return result
+def fill_mask(model: BertLMHeadModel, tokenizer: AutoTokenizer, query: str):
+    tokens = tokenizer(query)
+    input_ids = tokens["input_ids"]
+    yhat = model(
+        [
+            None,
+            torch.LongTensor([input_ids]),
+            torch.LongTensor([tokens["token_type_ids"]]),
+            torch.LongTensor([tokens["attention_mask"]]),
+            None,
+        ]
+    )
+    idx = yhat.argmax().item()
+    mask_pos = input_ids.index(tokenizer.mask_token_id)
+    input_ids[mask_pos] = idx
+    decoded_tok = tokenizer.decode([idx])
+    result = tokenizer.decode(input_ids)
+    return f"result: {result}, new token idx {idx}, new token decoded {decoded_tok}"
 
 
 for query in get_queries("[MASK]"):
+    config = PretrainedConfig.from_pretrained(ft_ckpt_dir)
+    model = BertLMHeadModel(config, args=ft_ckpt_dir)
+    model.from_pretrained(ft_ckpt_dir, args=ft_ckpt_dir)
+    tokenizer = AutoTokenizer.from_pretrained(ft_ckpt_dir)
+
     predicted = fill_mask(model, tokenizer, query)
     print(predicted)
-
