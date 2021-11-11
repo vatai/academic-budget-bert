282102ef09dd148077885371ac89965b7e6f606c
Emil VATAI <emil.vatai@gmail.com>
2021-11-11 17:25:33 +0900

    Unify dataset submission script IO

--- dataset/sub_merge.sh
+++ dataset/sub_merge.sh
@@ -5,16 +5,10 @@
 #$ -cwd
 #$ -j y
 
-source /etc/profile.d/modules.sh
-# module load gcc/9.3.0 python/3.8/3.8.7 openmpi/4.0.5 cuda/11.1/11.1.1 cudnn/8.0/8.0.5 nccl/2.8/2.8.4-1
-PYENV=pytorch
-source ~/${PYENV}.modules
-source ~/venv/${PYENV}/bin/activate
-
-OUTPUT_DIR=/groups/gcb50300/data/NLP/academic-budget-data/wikipedia
 PREV_JOB_NAME=sub_sharding.sh
-PREV_JOB_DIR=$(basename $(ls -d ${OUTPUT_DIR}/${PREV_JOB_NAME}.sh-* | sort | tail -n1))
-echo PREV_JOB_DIR=$PREV_JOB_DIR
+
+source common.src
+echo prev_job_dir=$(prev_job_dir $PREV_JOB_NAME)
 
 python3 merge_shards.py \
     --data ${OUTPUT_DIR}/${PREV_JOB_DIR}/output \
